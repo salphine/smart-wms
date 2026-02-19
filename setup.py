95af0f413db713fccaa1d5260a78dba378cd04fe
Your Name <your.email@example.com>
2026-02-19 11:35:26 +0300

    Simplify dependencies and add lazy loading

---
+++ setup.py
@@ -0,0 +1,14 @@
+﻿from setuptools import setup, find_packages
+
+setup(
+    name="smart-wms",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=[
+        "streamlit==1.22.0",
+        "pandas==1.5.3",
+        "numpy==1.23.5",
+        "plotly==5.14.0",
+        "requests==2.28.2",
+    ],
+)
