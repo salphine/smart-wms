dbf438c4572bd320405d34561f41c1f22d640bd1
Your Name <your.email@example.com>
2026-02-19 10:26:27 +0300

    Fix: Use stable package versions for Streamlit Cloud

---
+++ setup.sh
@@ -0,0 +1,11 @@
+﻿mkdir -p ~/.streamlit/
+echo "\
+[general]\n\
+email = \"salphine@example.com\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS = false\n\
+port = \\n\
+" > ~/.streamlit/config.toml
